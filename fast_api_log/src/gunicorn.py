3509b7754ef77e4e3fa903a5b8a5ff1a4bac9517
azazana <aazazana@gmail.com>
2022-09-02 19:58:01 +0300

    change gitignore

---
+++ fast_api_log/src/gunicorn.py
@@ -0,0 +1,13 @@
+"""Gunicorn config for FastAPI."""
+
+import multiprocessing
+
+name = "Gunicorn config for FastAPI"
+
+# accesslog = "/home/user/fastapi/gunicorn-access.log"
+# errorlog = "/home/user/fastapi/gunicorn-error.log"
+
+bind = "0.0.0.0:8000"
+
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = multiprocessing.cpu_count() * 2 + 1
